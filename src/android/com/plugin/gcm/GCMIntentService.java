8c73e49e5dc4fde0f7a3e57a8dd76e4f0e7bfb37
Stephan Klein <sklein.at@gmail.com>
2016-02-29 16:09:07 +0100

    Fix Icon

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -78,7 +78,7 @@ public class GCMIntentService extends GCMBaseIntentService {
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(context)
                         .setDefaults(defaults)
-                        .setSmallIcon(context.getApplicationInfo().icon)
+                        .setSmallIcon(R.drawable.app_icon)
                         .setWhen(System.currentTimeMillis())
                         .setContentTitle(extras.getString("title"))
                         .setTicker(extras.getString("title"))
